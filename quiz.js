bc41a4be45ff51374f955cdfb714e6d58076a7dc
Joshuawuro <joshuawuro@gmail.com>
2024-07-11 06:06:58 -0700

    JavaScript for the quiz

--- quiz.js
+++ quiz.js
@@ -2,8 +2,18 @@ function checkAnswer(){
     const correctAnswer = "4";
 }
 
-const selectedValue = document.querySelector("input[name='quiz']:checked")
-const selectedValu = document.querySelector("input[name='quiz']")
-console.log(selectedValu);
+const selectedValue = document.querySelector('input[name="quiz"]:checked');
 selectedValue.value = userAnswer;
 
+
+const feedback = document.getElementById('feedback');
+
+if (correctAnswer = userAnswer){
+    feedback.textContent = "Correct! Well done." 
+} else if (correctAnswer != userAnswer) {
+    feedback.textContent = "That's incorrect. Try again!"
+}
+
+const submitAnswer = document.getElementById('submit-answer');
+
+submitAnswer.addEventListener('click', checkAnswer);
