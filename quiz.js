824d5a045603bced4bfd8d9547f94f6793f2bdf2
Joshuawuro <joshuawuro@gmail.com>
2024-07-11 06:06:46 -0700

    JavaScript for the quiz

---
+++ quiz.js
@@ -0,0 +1,9 @@
+function checkAnswer(){
+    const correctAnswer = "4";
+}
+
+const selectedValue = document.querySelector("input[name='quiz']:checked")
+const selectedValu = document.querySelector("input[name='quiz']")
+console.log(selectedValu);
+selectedValue.value = userAnswer;
+
